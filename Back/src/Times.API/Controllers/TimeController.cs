2b077626453d1f53750a9b9bf524383f8ffb92e0
Alisson Youssf <youssfbr@gmail.com>
2021-09-01 23:32:00 -0300

    Projeto criado, controller, entidade

---
+++ Back/src/Times.API/Controllers/TimeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Times.API.Models;
+
+namespace Times.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]/classificacao_geral")]
+    public class CampeonatoController : ControllerBase
+    {
+        public IEnumerable<Time> _time = new Time[] 
+        {
+            new Time() {
+            TimeId = 1,
+            Jogada = "Test 1",
+            Resultado= "1 x 2"
+            },
+            new Time() {
+            TimeId = 2,
+            Jogada = "Test 3",
+            Resultado= "4 x 3"
+            }
+        };
+        public CampeonatoController()
+        {             
+        }
+
+        [HttpGet]
+        public IEnumerable<Time> Get()
+        {
+            return _time;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Time> GetById(int id)
+        {
+            return _time.Where(time => time.TimeId == id);
+        }
+
+        [HttpPost]
+        public bool Post(Time time) {
+
+            Console.WriteLine(time.TimeId);
+            Console.WriteLine(time.Jogada);
+            Console.WriteLine(time.Resultado);
+
+            return true;
+        }
+    }
+}
---
+++ Back/src/Times.API/Models/Time.cs
@@ -0,0 +1,9 @@
+namespace Times.API.Models
+{
+    public class Time
+    {        
+        public int TimeId { get; set; }
+        public string Jogada { get; set; }
+        public string Resultado { get; set; }
+    }
+}
